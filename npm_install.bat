7c29c6fe6232d8bb72e6a9a57d57105e63394805
cynynpri <pkmn_rgds@yahoo.co.jp>
2021-11-13 07:41:39 +0900

    improved node.js path.

--- npm_install.bat
+++ npm_install.bat
@@ -1,4 +1,4 @@
-@set path=Z:\bin_hub\win\node-v14.18.0-win-x64;Z:\bin_hub\win\node-v14.18.0-win-x64\node_modules;%PATH%
+@set path=Z:\path\to\node-v14.18.0-win-x64;Z:\path\to\node-v14.18.0-win-x64\node_modules;%PATH%
 
 npm init -y
 npm install -D selenium-webdriver mocha chromedriver
